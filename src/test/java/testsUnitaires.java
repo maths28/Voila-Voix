8117dc08415458f00d2a9ef96feb7766a61d0986
AlanVidal <vidal.travail@gmail.com>
2017-06-27 14:16:23 +0200

    Simplification du test unitaire

--- src/test/java/testsUnitaires.java
+++ src/test/java/testsUnitaires.java
@@ -25,28 +25,27 @@ public class testsUnitaires {
 		temoins.add("gpu");
 		Assert.assertEquals(temoins, TextTools.textSpliter("J'aime... aucuns les ordinateurs et les, GPU."));
 	}
-	
+
 	@Test
 	public void testBouchonné() throws IOException {
 
+		String theTxt = "Les ordinateurs ont des GPU des CPU ainsi que des périphériques "; 
+		List<String> wordList = TextTools.textSpliter(theTxt); // On le decoupe
+		System.out.println("TEXTE SPLITE" + wordList + "TEXTE SPLITE");
 
-				String theTxt = "Les ordinateurs ont des GPU des CPU ainsi que des périphériques "; //Le texte a analyser, phrase possible, mais moyen tant que textTools n'a pas corriger la section avec ecrit //PARTIE A CORRIGER ITERATOR
-				List<String> wordList = TextTools.textSpliter(theTxt); //On le decoupe
-				System.out.println("TEXTE SPLITE" + wordList + "TEXTE SPLITE");
-				
-				List<List<Definition>> listOfSynset = BabelSynsetsMoc.getSynsets(); //Bouchon
-				System.out.println("LISTE OF SYNSET " + listOfSynset +"LISTE OF SYNSET");
-				
-				//On recupere une list listant les valeurs de chaque elements definissant chaque mot du texte.
-				System.out.println("GENERATION DE TOKEN");
-				List<LinkedHashMap<String, Integer>> listOfHasMap = TokenGenerator.tokeningList(listOfSynset);
-				System.out.println(listOfHasMap);
-//				
-				Map<String, Integer> linkedMap = MapTools.fusion(listOfHasMap);
-				System.out.println(MapTools.sortByValues(MapTools.fusion(listOfHasMap)));
-				System.out.println("FINAL : " + MapTools.reduction(linkedMap));
-				Assert.assertEquals("{processeur=5, graphique=4, universit�=3, pr�sidents=3, conf�rence=3}",MapTools.reduction(linkedMap).toString());
-		
+		List<List<Definition>> listOfSynset = BabelSynsetsMoc.getSynsets(); // Bouchon
+		System.out.println("LISTE OF SYNSET " + listOfSynset + "LISTE OF SYNSET");
+
+		// On recupere une list listant les valeurs de chaque elements
+		// definissant chaque mot du texte.
+		System.out.println("GENERATION DE TOKEN");
+		List<LinkedHashMap<String, Integer>> listOfHasMap = TokenGenerator.tokeningList(listOfSynset);
+		System.out.println("test" + listOfHasMap);
+
+		Assert.assertEquals(
+				"[{processeur=5}, {processeur=5, graphique=5, conf�rence=4, pr�sidents=4, universit�=4}, {p�riph�rique=5, informatique=5, p�riph�riques=4, recueil=4}]",
+				listOfHasMap.toString());
+
 	}
 
 }
